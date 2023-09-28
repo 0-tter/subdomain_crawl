5217cfe053385e889c55cab401120a67feed23bb
0-tter <hsy157890@gmail.com>
2023-09-28 17:45:18 +0900

    End

---
+++ subdomain_crawl.py
@@ -0,0 +1,25 @@
+import re
+import requests
+import urllib.parse as urlparse
+
+target_url = "http://링크"
+target_links = []
+
+def extract_link_from(url):
+	response = requests.get(target_url)
+	return re.findall('(?:href=")(.*?)"', response.content.decode(errors="ignore"))
+
+def crawl(url):
+	href_links = extract_link_from(url)
+	for link in href_links:
+		link = urlparse.urljoin(url, link)
+
+		if "#" in link:
+			link = link.split("#")[0]
+
+		if target_url in link and link not in target_links:
+			target_links.append(link)
+			print(link)
+			crawl(link)
+
+crawl(target_url)
